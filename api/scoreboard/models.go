6c495935a831163b243fec915abf6e53c4099854
innovember <berikakhmed@gmail.com>
2021-03-12 22:49:30 +0600

    add initial setup for scoreboard

---
+++ api/scoreboard/models.go
@@ -0,0 +1,7 @@
+package scoreboard
+
+type ScoreBoard struct {
+	Nickname string `json:"nickname"`
+	Score    int64  `json:"score"`
+	Time     int64  `json:"time"`
+}
--- api/server.go
+++ api/server.go
@@ -1,1 +1,15 @@
-package api
+package main
+
+import (
+	"log"
+	"net/http"
+	//handlers "github.com/innovember/make-your-game/api/scoreboard"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	log.Println("Server is listening... http://localhost:8082/")
+	if err := http.ListenAndServe(":8082", mux); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
