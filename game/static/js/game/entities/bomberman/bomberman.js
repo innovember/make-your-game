1a87b46863ff98d06a1813fd4d3656703a7a43ef
innovember <berikakhmed@gmail.com>
2021-03-28 20:46:04 +0600

    update bomberman and entity classes

--- game/static/js/game/entities/bomberman/bomberman.js
+++ game/static/js/game/entities/bomberman/bomberman.js
@@ -1,45 +1,63 @@
-import { Entity } from '../entity.js'
+import { Entity } from "../entity.js"
+import { BOMBERMAN_DYING_TIME, PIXEL_SIZE } from "../../../utils/constants.js"
+import { Timer } from "../../../utils/timers/timer.js"
 
 export class Bomberman extends Entity {
-	constructor({board,liveCount}) {
-		super({board})
-		this.direction = 'down'
-		this.liveCount = liveCount
-	}
+  constructor({ board, liveCount }) {
+    super({ board })
+    this.left = 2 * PIXEL_SIZE
+    this.top = 2 * PIXEL_SIZE
+    this.direction = "down"
+    this.liveCount = liveCount
+  }
 
-	initialize = () => {
-		this.createHTML()
-	}
+  initialize = () => {
+    this.createHTML()
+  }
 
-	createHTML = () => {
-		this.div.id = 'bomberman'
-		this.img.src = './img/bomberman.png'
-		this.liveCountDiv = document.createElement('div')
-		this.liveCountDiv.id = 'live-count'
-		this.board.append(this.liveCountDiv)
-	}
+  createHTML = () => {
+    this.div.id = "bomberman"
+    this.img.src = "./img/bomberman.png"
+    this.img.alt = "bomberman"
+    this.liveCountDiv = document.createElement("div")
+    this.liveCountDiv.id = "live-count"
+    this.board.append(this.liveCountDiv)
+  }
 
-	moveLeft() {
-		super.moveLeft()
-		this.img.className = 'bomberman-walk-left'
-		this.direction = 'left'
-	}
+  moveLeft(speed) {
+    super.moveLeft(speed)
+    this.img.className = "bomberman-walk-left"
+    this.direction = "left"
+  }
 
-	moveRight() {
-		super.moveRight()
-		this.img.className = 'bomberman-walk-right'
-		this.direction = 'right'
-	}
+  moveRight(speed) {
+    super.moveRight(speed)
+    this.img.className = "bomberman-walk-right"
+    this.direction = "right"
+  }
 
-	moveUp() {
-		super.moveUp()
-		this.img.className = 'bomberman-walk-up'
-		this.direction = 'up'
-	}
+  moveUp(speed) {
+    super.moveUp(speed)
+    this.img.className = "bomberman-walk-up"
+    this.direction = "up"
+  }
 
-	moveDown() {
-		super.moveDown()
-		this.img.className = 'bomberman-walk-down'
-		this.direction = 'down'
-	}
+  moveDown(speed) {
+    super.moveDown(speed)
+    this.img.className = "bomberman-walk-down"
+    this.direction = "down"
+  }
+
+  resetPosition = () => {
+    this.left = PIXEL_SIZE * 2
+    this.top = PIXEL_SIZE * 2
+  }
+
+  die() {
+    this.img.className = "bomberman-die"
+    this.liveCount--
+    this.timer = new Timer(() => {
+      this.img.className = "bomberman-dead"
+    }, BOMBERMAN_DYING_TIME)
+  }
 }
--- game/static/js/game/entities/entity.js
+++ game/static/js/game/entities/entity.js
@@ -1,45 +1,51 @@
+import { PIXEL_SIZE, TILE_SIZE } from "../../utils/constants.js"
+
 export class Entity {
-    constructor(board, pixelSize, left, top, speed) {
-		this.board = board
-		this.pixelSize = pixelSize
-		this.speed = speed || pixelSize / 3
-		this.left = left || pixelSize * 2
-		this.top = top || pixelSize * 2
-		this.size = 16 * pixelSize * 0.75
+  constructor(board, left, top) {
+    this.board = board
+    this.pixelSize = pixelSize
+    this.speed = 1
+    this.left = left || PIXEL_SIZE * 2
+    this.top = top || PIXEL_SIZE * 2
+    this.size = TILE_SIZE * 0.75
 
-		this.createHTML()
-        this.draw()
-    }
+    this.createHTML()
+    this.draw()
+  }
 
-    createHTML = () => {
-        this.div = document.createElement('div')
-		this.div.style.position = 'absolute'
-		this.img = document.createElement('img')
-		this.div.append(this.img)
-		this.board.append(this.div)
-		this.div.style.height = `${this.size}px`
-		this.div.style.width = `${this.size}px`
-    }
-    
-    draw() {
-		this.div.style.position = 'absolute'
-		this.div.style.left = `${16 * this.pixelSize + this.left}px`
-		this.div.style.top = `${16 * this.pixelSize + this.top}px`
-	}
+  createHTML = () => {
+    this.div = document.createElement("div")
+    this.div.style.position = "absolute"
+    this.img = document.createElement("img")
+    this.div.append(this.img)
+    this.board.append(this.div)
+    this.div.style.height = `${this.size}px`
+    this.div.style.width = `${this.size}px`
+  }
 
-    moveLeft() {
-		this.left -= this.speed
-	}
+  draw() {
+    this.div.style.transform = `translate3d(${
+      TILE_SIZE + Math.floor(this.left)
+    }px, ${TILE_SIZE + Math.floor(this.top)}px, 0)`
+  }
 
-	moveRight() {
-		this.left += this.speed
-	}
+  moveLeft(speed) {
+    if (speed) this.left -= speed
+    else this.left -= this.speed
+  }
 
-	moveUp() {
-		this.top -= this.speed
-	}
+  moveRight(speed) {
+    if (speed) this.left += speed
+    else this.left += this.speed
+  }
 
-	moveDown() {
-		this.top += this.speed
-	}
+  moveUp(speed) {
+    if (speed) this.top -= speed
+    else this.top -= this.speed
+  }
+
+  moveDown(speed) {
+    if (speed) this.top += speed
+    else this.top += this.speed
+  }
 }
